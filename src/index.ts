498e4cea8f273a9ccaad4b76066df8dc4456992c
kravetsone <57632712+kravetsone@users.noreply.github.com>
2025-01-08 17:48:41 +0300

    feat: add .md writer

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { existsSync } from "node:fs";
+import { existsSync, writeFileSync } from "node:fs";
 import { Project } from "ts-morph";
 import { extractEventsFromSourceFiles } from "./utils.ts";
 
@@ -17,3 +17,23 @@ const typeChecker = project.getTypeChecker();
 const events = extractEventsFromSourceFiles(sourceFiles, typeChecker);
 
 console.log(events);
+
+let md = "";
+
+md += "## Events\n\n";
+
+for (const event of events) {
+    md += `- [${event.event}](#${event.event})\n`;
+}
+
+for (const event of events) {
+    md += `\n### ${event.event}\n\n`;
+
+    for (const property of event.properties) {
+        md += `- \`${property.name}\`: \`${property.type}\`\n`;
+    }
+
+    md += "\n\n";
+}
+
+writeFileSync("posthog.md", md);
